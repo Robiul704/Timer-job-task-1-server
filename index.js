2312a5b88b28303594c8d48c799730981fb4d373
robiul704 <robiulislam29935@gmail.com>
2023-12-22 16:19:46 +0600

    sir i can not deploy my job task for some error related essue.so that i moved my server code to previous project.then i update the deploy and run it.so forgive me please

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const cors=require('cors')
+require('dotenv').config()
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://robiul:robiulislam@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, {
@@ -22,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
    
-    await client.connect();
+    // await client.connect();
 
     const productcollection=client.db('productDB').collection('product')
     const brandcollection=client.db('productDB').collection('Brand')
@@ -124,8 +126,67 @@ async function run() {
       res.send(result)
     })
    
-    
-    await client.db("admin").command({ ping: 1 });
+    //---------------------------------------------------------
+    //-------------------job task 1-------------------------
+    //---------------------------------------------------------
+    const ToDoCollection=client.db('DragAndDrop').collection('ToDoCollection')
+    const OnGoingCollection=client.db('DragAndDrop').collection('OnGoingCollection')
+    const CompliteCollection=client.db('DragAndDrop').collection('CompliteCollection')
+   
+    //-------------------------------------------------------------------
+    app.post('/todo',async(req,res)=>{
+      const data=req.body
+      const result=await ToDoCollection.insertOne(data)
+       res.send(result)
+    })
+    app.post('/ongoing',async(req,res)=>{
+      const data=req.body
+      const result=await OnGoingCollection.insertOne(data)
+       res.send(result)
+    })
+    app.post('/complite',async(req,res)=>{
+      const data=req.body
+      const result=await CompliteCollection.insertOne(data)
+       res.send(result)
+    })
+//---------------------------------------------------------------------------
+      app.get('/todo',async(req,res)=>{
+        const result=await ToDoCollection.find().toArray()
+        res.send(result)
+      })
+      app.get('/ongoing',async(req,res)=>{
+        const result=await OnGoingCollection.find().toArray()
+        res.send(result)
+      })
+      app.get('/complite',async(req,res)=>{
+        const result=await CompliteCollection.find().toArray()
+        res.send(result)
+      })
+    //------------------------------------------------------------
+       app.delete('/todo/:id',async (req,res)=>{
+        const id=req.params.id
+        const filter={_id: new ObjectId(id)}
+        const result=await ToDoCollection.deleteOne(filter)
+        res.send(result)
+       })
+       app.delete('/ongoing/:id',async (req,res)=>{
+        const id=req.params.id
+        const filter={_id: new ObjectId(id)}
+        const result=await OnGoingCollection.deleteOne(filter)
+        res.send(result)
+       })
+       app.delete('/complite/:id',async (req,res)=>{
+        const id=req.params.id
+        const filter={_id: new ObjectId(id)}
+        const result=await CompliteCollection.deleteOne(filter)
+        res.send(result)
+       })
+       //---------------------------------------------------
+       //--------------------------------------------------
+       //------------------------------------------------
+
+
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
